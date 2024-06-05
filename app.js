44ed0d29b8e5411f6e4289f032c7388bef26d613
HectorOrtiz789 <hector.ortiz4@unmsm.edu.pe>
2024-06-05 17:40:08 -0500

    Webserver

--- app.js
+++ app.js
@@ -33,6 +33,13 @@ app.get('/generic', (req, res) => {
   });
 });
 
+app.get('/salidas', (req, res) => {
+  res.render('salidas', {
+      nombre: 'Armando David Espinoza',
+      titulo: 'Curso de Node'
+  });
+});
+
 app.get('*', (req, res) => {
   res.sendFile(__dirname + '/public/404.html');
 });
